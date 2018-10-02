15c84e280377e37e2dbd0444b43dbb5388ff8666
TejasD10 <Welcome@1090>
2018-10-02 17:51:27 -0400

    CTCI - Chap_4 - first_common_ancestor

---
+++ data_structures/CTCI - Chap_4/first_common_ancestor.py
@@ -0,0 +1,98 @@
+import unittest
+
+
+class Node:
+    """
+    Node class with a pointer to its parent node
+    """
+
+    def __init__(self, data):
+        self.left = self.right = self.parent = None
+        self.data = data
+
+    def __str__(self):
+        return str(self.data)
+
+
+def remove_items(diff, node_list):
+    if diff == 0:
+        return
+    for i in range(diff):
+        node_list.pop(0)
+
+
+def first_common_ancestor(root, v1, v2):
+    """
+    Return the first common ancestor of the nodes containing v1 and v2 in its data.
+    :param v1: element 1 present in the tree
+    :param v2: element 2 present in the tree
+    :return: Value of the first common ancestor
+    """
+    if not root:
+        return
+
+    # Find the nodes in the tree
+    node_v1 = search(root, v1)
+    node_v2 = search(root, v2)
+
+    # Remove the extra nodes from the path
+    diff = abs(len(node_v1) - len(node_v2))
+    remove_items(diff, node_v1 if len(node_v1) > len(node_v2) else node_v2)
+    # One of the nodes not found
+    if not node_v1 or not node_v2:
+        return
+
+    # Keep popping elements from the returned lists until you get the same element or
+    # you run out of elements in one list
+    while node_v1 and node_v2:
+        if node_v1[0] == node_v2[0]:
+            return node_v1[0]
+        node_v1.pop(0)
+        node_v2.pop(0)
+
+
+def search(root, v1):
+    """
+    Search for values v1 and v2 in the tree
+    :param root: root of the tree to be searched
+    :param v1: search the value v1
+    :param v2: search the value v1
+    :return: Reference to the nodes containing values v1 and v2
+    """
+    if not root:
+        return
+
+    if root.data == v1:
+        return [root.data]
+    left = search(root.left, v1)
+    if left:
+        left.append(root.data)
+        return left
+    right = search(root.right, v1)
+    if right:
+        right.append(root.data)
+        return right
+
+
+class TestFirstCommonAncestor(unittest.TestCase):
+    def setUp(self):
+        self.root = Node(1)
+        self.root.left = Node(2)
+        self.root.left.parent = self.root
+        self.root.left.left = Node(4)
+        self.root.left.left.parent = self.root.left
+        self.root.left.right = Node(5)
+        self.root.left.right.parent = self.root.left
+
+        self.root.right = Node(3)
+        self.root.right.parent = self.root
+        self.root.right.left = Node(6)
+        self.root.right.left.parent = self.root.right
+        self.root.right.right = Node(7)
+        self.root.right.right.parent = self.root.right
+
+    def test_first_common_ancestor(self):
+        self.assertEqual(2, first_common_ancestor(self.root, 4, 5))
+        self.assertEqual(1, first_common_ancestor(self.root, 4, 6))
+        self.assertEqual(1, first_common_ancestor(self.root, 4, 3))
+        self.assertEqual(2, first_common_ancestor(self.root, 4, 2))
